6a7e81bf7b905fa5cfd972620c1885517979fd87
Vadim <vadimbaranivsky83@gmail.com>
2022-01-14 12:41:01 +0200

    log

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -65,7 +65,7 @@ function App({logout, editUser}) {
       }]
     }).then(async () => await window.ethereum.request({
       method: 'eth_requestAccounts'
-    })).catch(err => (JSON.stringify(err)))
+    })).catch(err => alert(JSON.stringify(err)))
     const account = accounts[0]
     user.currentAccount = account
     setUser({ ...user })
